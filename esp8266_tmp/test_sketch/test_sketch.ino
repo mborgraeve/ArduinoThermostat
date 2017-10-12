20916a14c6d0101300f5aca4a939cecbe73d8c51
mborgraeve <matthieu.borgraeve@gmail.com>
2017-10-12 17:55:11 -0400

    working t and h with low precision and return on serial

--- esp8266_tmp/test_sketch/test_sketch.ino
+++ esp8266_tmp/test_sketch/test_sketch.ino
@@ -3,23 +3,24 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <PString.h>
-//#include <DHT.h>
+#include <DHT.h>
 
 
-//#define DHT11_PIN 2
-//#define DHTTYPE DHT11
-//DHT dht(DHT11_PIN, DHTTYPE,11);
+#define DHT11_PIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHT11_PIN, DHTTYPE,11);
 const char* ssid = "BandelBorgraeveBGN"; // remplacer par le SSID de votre WiFi
 const char* password = "coucoucnous"; // remplacer par le mot de passe de votre WiFi
 ESP8266WebServer server(80); // on instancie un serveur ecoutant sur le port 80
-//float t, h;
+float t, h;
 //const int average_loop = 300;
 //char answerBuffer[5000];
 
 void setup(void){
   Serial.println("Initializing...");
   Serial.begin(115200);
-  
+  pinMode(DHT11_PIN, INPUT);
   // on demande la connexion au WiFi
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -32,53 +33,28 @@ void setup(void){
   Serial.println("");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  // on definit ce qui doit etre fait lorsque la route /bonjour est appelee
-  // ici on va juste repondre avec un "hello !"
-  server.on("/bonjour", [](){
-    server.send(200, "text/plain", "hello !");
-    Serial.print("hello !");
-  });
-  pinMode(2, OUTPUT);
-  server.on("/2/on", [](){
-    digitalWrite(2,HIGH);
-    server.send(200, "text/plain", "LED 2 turned on !");
-    Serial.print("LED 2 turned on !");
-  });
-  server.on("/2/off", [](){
-    digitalWrite(2,LOW);
-    server.send(200, "text/plain", "LED 2 turned off !");
-    Serial.print("LED 2 turned off !");
-  });
-  //server.on("/2/read", [](){
-  //  PString answer(answerBuffer, sizeof(answerBuffer));
-  //  average=0;
-  //  answer.print("Read:");
-  //  for(int i = 0; i<average_loop;i++){
-  //    int chk = DHT.read11(DHT11_PIN);
-  //    answer.print("T:");answer.print(DHT.temperature);
-  //    answer.print(",H:");answer.print(DHT.humidity);
-  //    answer.print(";");
-  //    average = average + DHT.temperature;
-  //  }
-  //  answer.print("\n");
-   // average = average / average_loop;
-   // answer.print("Average:");
-  //  answer.print(average);
-  //  Serial.print(answerBuffer);
-  //  server.send(200, "text/plain", answerBuffer);
-  //});
-  // on commence a ecouter les requetes venant de l'exterieur
-  //server.on("/all", [](){
-  //  PString answer(answerBuffer, sizeof(answerBuffer));
-  //  h = dht.readHumidity();          // Read humidity (percent)
-  //  t = dht.readTemperature(false);     // Read temperature
+  Serial.print("Wait 2s for DHT initialization");
+  for(int i = 0;i<4;i++){delay(500);Serial.print(".");i++;}
+  Serial.println(" Done!");
+  
+   //on commence a ecouter les requetes venant de l'exterieur
+  server.on("/all", [](){
+  //PString answer(answerBuffer, sizeof(answerBuffer));
+    h = dht.readHumidity();          // Read humidity (percent)
+    t = dht.readTemperature(false);     // Read temperature
   //  answer.print("T:");
   //  answer.print(t);
   //  answer.print(",H:");
   //  answer.print(h);
   //  answer.print("\n");
   //  server.send(200, "text/plain", answerBuffer);
-  //});
+    server.send(200, "text/plain", "Data read sent to serial");
+    Serial.print("T:");
+    Serial.print(h);
+    Serial.print(",H:");
+    Serial.print(t);
+    Serial.println(";");
+  });
   server.begin();
 }
 void loop(void){
