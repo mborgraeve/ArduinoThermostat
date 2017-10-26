4220a531e97da9911d13becff8e0c683dd31406a
mborgraeve <matthieu.borgraeve@gmail.com>
2017-10-26 08:02:20 -0400

    example working in arduino with NTP getting UTC time

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,88 +1,149 @@
-// Include library found at 
-// https://docs.google.com/file/d/0Bwrp4uluZCpNdE9oWTY0M3BncTA/edit 
-#include "SevSeg.h"
+/*
+ Udp NTP Client
+ Get the time from a Network Time Protocol (NTP) time server
+ Demonstrates use of UDP sendPacket and ReceivePacket
+ For more on NTP time servers and the messages needed to communicate with them,
+ see http://en.wikipedia.org/wiki/Network_Time_Protocol
+ created 4 Sep 2010
+ by Michael Margolis
+ modified 9 Apr 2012
+ by Tom Igoe
+ updated for the ESP8266 12 Apr 2015 
+ by Ivan Grokhotkov
+ This code is in the public domain.
+ */
 
-#include <math.h>
-#define ANALOG_IN 0
-#define DRIVER_OUT 2
-#define TEMP_IN 5
-#define ALPHA 0.0001
-#define INTERVAL 5000
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+char ssid[] = "BandelBorgraeveBGN";  //  your network SSID (name)
+char pass[] = "coucoucnous";       // your network password
 
 
-//Create an instance of the object.
-SevSeg sevseg;
-double temperature=0.;
-double smoothx = 0;
-unsigned long last_time = millis();
-unsigned long current_time = 0;
-unsigned int count =0;
+unsigned int localPort = 2390;      // local port to listen for UDP packets
 
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+ *  Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
 
-void setup() {
-  //Serial.begin(9600);
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+void setup()
+{
   Serial.begin(115200);
-pinMode(DRIVER_OUT,OUTPUT);
-    // Set up pins
-  // Arguments : 
-  // First 0 is for common cathode
-  // Following 0 to 11 numbers are for Arduino pins
-  // connected to display in the following order
-  // 1,2,3,4,A,B,C,D,E,F,G,DP
-  //byte digitPins[] ={2,3,4,5};
-  //byte segmentPins[]={13,11,7,8,10,12,6,9};
-  //sevseg.begin(COMMON_ANODE,4,digitPins,segmentPins);
-  //sevseg.setBrightness(10);
-}
+  Serial.println();
+  Serial.println();
 
-void thermistor(int RawADC) {
-  double Temp;
-  Temp = log(10000.0 * ((1024.0 / RawADC - 1)));
-  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
-  Temp = Temp - 273.15;            // Convert Kelvin to Celcius
-  //Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-  temperature= Temp;
-  //temperature = temperature*(1-ALPHA)+Temp*10.*ALPHA;
-}
-void loop() {
-current_time=millis();
-  /*if(current_time-last_time > INTERVAL){
-    count++;
-    last_time = current_time;
-    if(count%2){
-      digitalWrite(DRIVER_OUT, HIGH);
-    }else{digitalWrite(DRIVER_OUT, LOW);}
-  }*/
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
   
-  //thermistor(analogRead(ANALOG_IN));
-  //int tint = (int) temperature;
-  //int tint = (int) analogRead(ANALOG_IN);
-  //int tint = analogRead(ANALOG_IN);
-  int tint = analogRead(ANALOG_IN);
-  thermistor(tint);
-    //Serial.write( 0xff );
-  //Serial.write( (tint >> 8) & 0xff );
-  //Serial.write( tint & 0xff );
-  Serial.print("\n");
-  Serial.print(tint);
-  //Serial.print("t:");
-  //Serial.print(temperature);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
+}
+
+void loop()
+{
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP); 
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
 
 
-  // Prepare number to display, and dot position 
-  // (0 to 3, other number means no dot)  
-  //sevseg.setNumber(tint);
-  // Display number 
-  //sevseg.refreshDisplay();
-  //delay(500);
-  // To maintain display with this wiring, 
-  // we are forced to loop over those commands rapidly
-  //delay(500);
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+  // wait ten seconds before asking for the time again
+  delay(10000);
+}
 
-  //sevseg.refreshDisplay(); // Must run repeatedly
-  /*int val = analogRead(ANALOG_IN);
-  Serial.write( 0xff );
-  Serial.write( (val >> 8) & 0xff );
-  Serial.write( val & 0xff );*/
-  //delay(1000);
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
